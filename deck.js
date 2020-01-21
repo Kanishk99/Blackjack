23c723680ca274c090ce4539737d1bb94e44748a
Kanishk99 <kanishkkargutkar123@gmail.com>
2020-01-21 16:48:08 +0530

    test

--- deck.js
+++ deck.js
@@ -2,6 +2,7 @@ deck = new Array();
 suits = ['spades','diamonds','clubs','hearts'];
 ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
+/*test*/
 function createDeck()
 {
   for(suitsIndex=0;suitsIndex<suits.length;suitsIndex++)
