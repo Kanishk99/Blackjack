c44a78e4ac7a8a5e0a5493f18bdbf8c98e2ccc60
Kanishk99 <kanishkkargutkar123@gmail.com>
2020-02-16 23:28:15 +0530

    Changed bust check

--- deck.js
+++ deck.js
@@ -1,5 +1,5 @@
 deck = new Array();
-suits = ["spades", "diamonds", "clubs", "hearts"];
+suits = ["S", "D", "C", "H"];
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 playerTotal = 0;
 dealerTotal = 0;
@@ -22,11 +22,13 @@ function shuffle(array) {
 }
 
 function createDeck() {
+  var img = document.createElement("img");
   for (suitsIndex = 0; suitsIndex < suits.length; suitsIndex++) {
     for (ranksIndex = 0; ranksIndex < ranks.length; ranksIndex++) {
       card = {};
       card.suit = suits[suitsIndex];
       card.rank = ranks[ranksIndex];
+      //card.img.src = "D:\Kanishk\Desktop\git\Blackjack\Cards PNG\\" + card.rank + card.suit + ".png";
       card.value = setValue();
       deck.push(card);
     }
@@ -50,11 +52,12 @@ function disableButtons(bool) {
   document.getElementById("hit").disabled = bool;
   document.getElementById("stand").disabled = bool;
 }
-/*test*/
+
 function checkBustPlayer() {
   if (playerTotal > 21) {
-    document.getElementById("playerSide").innerHTML += "<BR> BUST!";
+    //document.getElementById("playerSide").innerHTML += "<BR> BUST!";
     disableButtons(true);
+    checkVictory();
     return true;
   } else {
     return false;
@@ -63,7 +66,8 @@ function checkBustPlayer() {
 
 function checkBustDealer() {
   if (dealerTotal > 21) {
-    document.getElementById("playerSide").innerHTML += "<BR>DEALER BUST!";
+    //document.getElementById("playerSide").innerHTML += "<BR>DEALER BUST!";
+    checkVictory();
     return true;
   } else {
     return false;
@@ -84,8 +88,8 @@ function playerTurn() {
   playerHand();
   divPlayer = "playerSide" + divPlayerIncrement;
   document.getElementById(divPlayer).innerHTML += "<BR>";
-  document.getElementById(divPlayer).innerHTML +=
-    playerCards.rank + playerCards.suit;
+  document.getElementById(divPlayer).innerHTML += playerCards.rank + playerCards.suit;
+  //src.appendChild(img);
   document.getElementById(divPlayer).innerHTML += " Total : " + playerTotal;
   divPlayerIncrement++;
 }
@@ -111,8 +115,7 @@ function dealerTurn() {
   dealerHand();
   divDealer = "dealerSide" + divDealerIncrement;
   document.getElementById(divDealer).innerHTML += "<BR>";
-  document.getElementById(divDealer).innerHTML +=
-    dealerCards.rank + dealerCards.suit;
+  document.getElementById(divDealer).innerHTML += dealerCards.rank + dealerCards.suit;
   document.getElementById(divDealer).innerHTML += " Total : " + dealerTotal;
   divDealerIncrement++;
 }
@@ -120,7 +123,7 @@ function dealerTurn() {
 function checkVictory() {
   if (playerTotal > dealerTotal && !checkBustPlayer()) {
     document.getElementById("victory").innerHTML += "Player wins!";
-  } else if (dealerTotal > playerTotal && !checkBustDealer()) {
+  } else if (dealerTotal > playerTotal && (!checkBustDealer() || checkBustPlayer())) {
     document.getElementById("victory").innerHTML += "Dealer wins!";
   } else if (playerTotal == dealerTotal) {
     document.getElementById("victory").innerHTML += "PUSH!";
