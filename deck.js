b7f3bfa6632e6192a820b67493c4b3006ecb93d3
Kanishk K <42405147+Kanishk99@users.noreply.github.com>
2020-09-09 18:00:30 +0530

    Add files via upload
    Fixed again

--- deck.js
+++ deck.js
@@ -22,13 +22,13 @@ function shuffle(array) {
 }
 
 function createDeck() {
-  var img = document.createElement("img");
+  //var img = document.createElement("img");
   for (suitsIndex = 0; suitsIndex < suits.length; suitsIndex++) {
     for (ranksIndex = 0; ranksIndex < ranks.length; ranksIndex++) {
       card = {};
       card.suit = suits[suitsIndex];
       card.rank = ranks[ranksIndex];
-      //card.img.src = "D:\Kanishk\Desktop\git\Blackjack\Cards PNG\\" + card.rank + card.suit + ".png";
+      card.imagePath = "Cards PNG\\" + card.rank + card.suit + ".png";
       card.value = setValue();
       deck.push(card);
     }
@@ -79,19 +79,24 @@ function playerHand() {
   if (!checkBustPlayer()) {
     playerCards = deck.pop();
     playerTotal += playerCards.value;
-    checkBustPlayer();
   }
-  console.log(playerCards.suit);
+}
+
+function setImage(img, setDiv){
+  cardImage = document.createElement("img");
+  cardImage.src = img;
+  document.getElementById(setDiv).appendChild(cardImage);
 }
 
 function playerTurn() {
   playerHand();
   divPlayer = "playerSide" + divPlayerIncrement;
-  document.getElementById(divPlayer).innerHTML += "<BR>";
-  document.getElementById(divPlayer).innerHTML += playerCards.rank + playerCards.suit;
-  //src.appendChild(img);
+  setImage(playerCards.imagePath,divPlayer);
+  /*document.getElementById(divPlayer).innerHTML += "<BR>";
+  document.getElementById(divPlayer).innerHTML += playerCards.rank + playerCards.suit;*/
   document.getElementById(divPlayer).innerHTML += " Total : " + playerTotal;
   divPlayerIncrement++;
+  checkBustPlayer();
 }
 
 function playerStand() {
@@ -114,20 +119,32 @@ function dealerHand() {
 function dealerTurn() {
   dealerHand();
   divDealer = "dealerSide" + divDealerIncrement;
-  document.getElementById(divDealer).innerHTML += "<BR>";
-  document.getElementById(divDealer).innerHTML += dealerCards.rank + dealerCards.suit;
+  setImage(dealerCards.imagePath,divDealer);
+  /*document.getElementById(divDealer).innerHTML += "<BR>";
+  document.getElementById(divDealer).innerHTML += dealerCards.rank + dealerCards.suit;*/
   document.getElementById(divDealer).innerHTML += " Total : " + dealerTotal;
   divDealerIncrement++;
+  checkBustDealer();
 }
 
 function checkVictory() {
-  if (playerTotal > dealerTotal && !checkBustPlayer()) {
+  if (playerTotal>21)
+  {
+    document.getElementById("victory").innerHTML += "Dealer wins!";
+  }
+  else if (dealerTotal>21)
+  {
     document.getElementById("victory").innerHTML += "Player wins!";
-  } else if (dealerTotal > playerTotal && (!checkBustDealer() || checkBustPlayer())) {
+  }
+  else if (playerTotal > dealerTotal && (!checkBustPlayer()))  {  
+    document.getElementById("victory").innerHTML += "Player wins!";
+  } else if (dealerTotal > playerTotal && (!checkBustDealer())) {
     document.getElementById("victory").innerHTML += "Dealer wins!";
   } else if (playerTotal == dealerTotal) {
     document.getElementById("victory").innerHTML += "PUSH!";
-  }
+    //Game resets -> Deal beings again with previous money added to total
+  } 
+
 }
 
 function startGame() {
@@ -136,4 +153,8 @@ function startGame() {
   shuffle(deck);
   dealerTurn();
   playerTurn();
+  dealerTurn();
+  playerTurn();
 }
+
+
