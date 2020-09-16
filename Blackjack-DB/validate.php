df2d023ec3fcf3374f1f63b35ab61c9428fde5e1
Kanishk K <42405147+Kanishk99@users.noreply.github.com>
2020-09-16 21:52:30 +0530

    Add files via upload

---
+++ Blackjack-DB/validate.php
@@ -0,0 +1,79 @@
+<?php
+
+
+
+session_start();
+
+$connect = mysqli_connect("localhost","root","","blackjackUD"); # connection to db
+// if($connect) 
+// {
+//  echo " connection established";
+// }
+// else
+// {    
+//     echo " ERROR NOT CONNECTED";
+// } # just  to check the connection between php and data  base to verify the connectionp;
+$name= $_POST['nm'];# name of the user using for the login 
+$Password= $_POST['userpasswrord']; # PASSWORD user for the login
+// $a=$_POST['address'];
+
+$name= mysqli_real_escape_string($connect,$name);# to prevent from sql injection secured form
+$Password= mysqli_real_escape_string($connect,$Password);# to prevent from sql injection secured form
+
+ $query = "select  user_password from user_details where user_name = '$name'";
+//$check = " select * from user_details where user_name= '$name' && user_password = '$Password'"; # query to chek the record exist or not in the databse
+$result = mysqli_query($connect,$query) ; 
+//$check_rows = mysqli_num_rows($result);# l1 p1
+ $check_rows = mysqli_fetch_assoc($result); # check the no of rows in the database
+//echo $check_rows['user_password']; # check the encrpted pasword from the database
+
+ $pass_chk= $check_rows['user_password'];
+
+
+
+if(password_verify($Password,$pass_chk))
+{
+   
+    echo $_SESSION['username']=$name;
+    echo "<script> 
+    alert('LOGIN SUCESSFUL');
+    location.href='mainpage.php'; 
+    </script>";
+
+}
+else
+{
+    echo "<script> 
+    alert('LOGIN Failed Please check ID or Password');
+    location.href='login.html'; 
+    </script>";
+}
+
+
+
+
+
+// if ($check_rows == 1) #check the records in the table 
+// {
+//     // echo " RECORD IS PRESENT  ";
+
+//     $_SESSION['username']=$name;
+//     echo "<script> 
+//     alert('LOGIN SUCESSFUL');
+//     location.href='mainpage.php'; 
+//     </script>";
+//     //header('location:newglowing.html');
+// }
+// else
+// {
+//     echo "<script> 
+//     alert('LOGIN Failed Please check ID or Password');
+//     location.href='mainpage.php'; 
+//     </script>";
+    
+//   // header('location:login.html'); 
+    
+// }
+
+
+?>
---
+++ Database/login.php
@@ -0,0 +1,44 @@
+<?php
+
+$name=$_POST['nm'];# 
+// $i=$_POST['userid'];
+$Password=$_POST['userpasswrord'];
+$Email=$_POST['address'];
+
+echo"Welcome $name";
+echo "<br>";
+//echo "YOUR ID: $i";
+// echo "Password: $p";
+// echo "<br>";
+// echo "Address: $a"; # cheking the input getting in php or not 
+// echo " <br>";
+// echo "<br>";
+
+ $connect = mysqli_connect("localhost","root","","userdata");
+// if($connect) 
+// {
+//  echo " connection established";
+// }
+// else
+// {    
+//     echo " ERROR NOT CONNECTED";
+// } # just  to check the connection between php and data  base to verify the connectionp;
+
+ $query = "INSERT INTO `user_details`( `user_name`, `user_address`, `user_password`) VALUES ('$name','$Email','$Password')";
+ $execute = mysqli_query($connect,$query); # to execute the query its function with 2 parameter 1 dtabsename,queryname
+
+ # checking the execution of the query
+
+ if($query == true)
+ {
+     echo " RECORD INSERTED ...SUCESSFULLY";
+ }
+ else
+ {
+     echo "! ERROR RECORD INSERTED ...UNSUCESSFULLY ! ";
+ }
+
+ # to retive the data database
+ 
+
+?>
